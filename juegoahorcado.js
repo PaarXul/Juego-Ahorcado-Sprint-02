b28bbe5dfe9584ac9920a59db4fe2a56bfaf29f6
PaarXul <gian.parra96@gmail.com>
2022-09-05 01:22:23 -0300

    Funcionalidades terminadas falta ajustar, lo visua

---
+++ juegoahorcado.js
@@ -0,0 +1,200 @@
+var palabras = Array("Barcelona", "Santiago", "Brasilia");
+let palabraAdi = "";
+let palabraOculta = "";
+let contadorVida = 6;
+let letras_erradas = "";
+let agregarpalabra = "";
+let palabrasretenidas = "";
+
+function iniciar() {
+  //Inicia el random de palabras. y muestra el tamaño de la palabra a adivinar.
+  palabraOculta = palabras[Math.floor(Math.random() * palabras.length)];
+  for (let i = 0; i < palabraOculta.length; i++) {
+    palabraAdi = palabraAdi + "_ ";
+  }
+  document.getElementById("frase").innerHTML = palabraAdi;
+}
+iniciar();
+
+let letra = "";
+let code = "";
+window.addEventListener(
+  "keydown",
+  function (event) {
+    letra = event.key;
+    code = event.keyCode;
+    resultado();
+    LetraM(letra);
+  },
+  false
+);
+function comprobar() {
+  letra = letra.toUpperCase();
+
+  palabraOculta = palabraOculta.toUpperCase();
+  let nuevo = "";
+
+  for (let i = 0; i < palabraOculta.length; i++) {
+    //itera la palabra y busca por medio de la letra ingresada.
+    if (letra == palabraOculta[i]) {
+      nuevo = nuevo + letra + " ";
+    } else {
+      nuevo = nuevo + palabraAdi[i * 2] + " ";
+    }
+  }
+
+  if (nuevo == palabraAdi) {
+    //compara el resultado, si el resultado es igual se resta la vida. y añade la letra ingresada.
+    contadorVida--;
+    letras_erradas = letras_erradas + letra + " ";
+    document.getElementById("letrasErroneas").innerHTML = letras_erradas;
+    document.getElementById("vida").innerHTML =
+      "Vidas Actuales: " + contadorVida;
+  }
+
+  palabraAdi = nuevo;
+  //Inserta la palabra adivinada en el div de frase adivinada.
+  document.getElementById("frase").innerHTML = palabraAdi;
+
+  if (contadorVida < 1) {
+    //si el contador de vida es menor a uno pierde
+    document.getElementById("vida").innerHTML = "Perdiste";
+    document.getElementById("vida").classList = "perdiste";
+    document.getElementById("vida").style.fontSize = "20px";
+  }
+
+  if (palabraAdi.search("_") == -1) {
+    //busca en la palabra adivinada el guion bajo si este es menor a menos 1 se refleja el resultado ganador.
+    document.getElementById("vida").innerHTML = "Ganaste";
+    document.getElementById("vida").style.fontSize = "20px";
+  }
+  dibujar();
+  letra = "";
+}
+
+function dibujar() {
+  var canvas = document.getElementById("lienzo");
+  if (canvas.getContext) {
+    var ctx = canvas.getContext("2d");
+    //Dibuja la base del ahorcado
+    if (contadorVida <= 5) {
+      ctx.beginPath();
+      ctx.moveTo(0, 150);
+      ctx.lineTo(150, 150);
+      ctx.stroke();
+    }
+
+    if (contadorVida <= 4) {
+      //dibuja el poste
+      ctx.beginPath();
+      ctx.moveTo(30, 200);
+      ctx.lineTo(30, 10);
+      ctx.lineTo(150, 10);
+      ctx.lineTo(150, 20);
+      ctx.stroke();
+    }
+
+    if (contadorVida <= 3) {
+      //Dibujo Cabeza
+      ctx.beginPath();
+      ctx.arc(150, 40, 20, 0, Math.PI * 2);
+      ctx.stroke();
+    }
+
+    if (contadorVida <= 2) {
+      //dibujar cuerpo
+      ctx.beginPath();
+      ctx.moveTo(150, 60);
+      ctx.lineTo(150, 100);
+      ctx.stroke();
+    }
+
+    if (contadorVida <= 1) {
+      //Dibujar Brazos
+      ctx.beginPath();
+      ctx.moveTo(150, 60);
+      ctx.lineTo(130, 100);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(150, 60);
+      ctx.lineTo(170, 100);
+      ctx.stroke();
+    }
+    if (contadorVida == 0) {
+      ctx.beginPath();
+      ctx.moveTo(150, 100);
+      ctx.lineTo(170, 130);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(150, 100);
+      ctx.lineTo(130, 130);
+      ctx.stroke();
+    }
+  }
+}
+
+function checkName(evento) {
+  var charCode = evento;
+  if (charCode != 0) {
+    if (charCode < 65 || charCode > 90 == 192) {
+      //HACK DEFINITIVO PARA ADMITIR SOLO LETRAS
+    } else {
+      LetraRepetida(letra);
+    }
+  }
+}
+
+function LetraM(string) {
+  //solo letras MAYUSCULAS
+  var out = "";
+  //Se añaden las letras validas
+  var filtro = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"; //Caracteres validos
+  for (var i = 0; i < string.length; i++)
+    if (filtro.indexOf(string.charAt(i)) != -1) checkName(code);
+}
+
+function LetraRepetida(LetraIngresada) {
+  if (
+    palabraAdi.includes(LetraIngresada) ||
+    letras_erradas.includes(LetraIngresada)
+  ) {
+    //RETORNA SIN MOSTRAR UNA NUEVA LETRA INGRESADA
+    return false;
+  }
+  comprobar();
+}
+
+function resultado() {
+  //COMPRUEBA EL RESULTADO ACTUAL
+  if (contadorVida < 1) {
+    //si el contador de vida es menor a uno pierde
+    document.removeEventListener("keydown", KeyboardEvent);
+    return;
+  }
+  if (palabraAdi.search("_") == -1) {
+    //busca en la palabra adivinada el guion bajo si este es menor a menos 1 se refleja el resultado ganador.
+    document.removeEventListener("keydown", KeyboardEvent);
+    return;
+  }
+}
+function nuevaPalabra() {
+  agregarpalabra = document.querySelector(".nuevapalabrajuego").value;
+
+  palabras.push(agregarpalabra);
+
+  document.getElementById("nuevapalabrajuego").value = "";
+
+  mostrarpalabrasingresadas();
+
+  return;
+}
+
+function mostrarpalabrasingresadas() {
+  palabrasretenidas = "";
+  for (let i = 0; i < palabras.length; i++) {
+    //itera la palabra y busca por medio de la letra ingresada.
+    palabrasretenidas = palabrasretenidas + palabras[i] + " ";
+  }
+
+  document.getElementById("palabrasingresadas").innerHTML = palabrasretenidas;
+}

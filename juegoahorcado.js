be3fd54be45ad1e8bb211726e943e5708aac7ee5
PaarXul <gian.parra96@gmail.com>
2022-09-16 00:44:21 -0300

    Validación campo entrada.

--- juegoahorcado.js
+++ juegoahorcado.js
@@ -35,6 +35,7 @@ window.addEventListener(
   },
   false
 );
+
 function comprobar() {
   letra = letra.toUpperCase();
 
@@ -196,6 +197,7 @@ function resultado() {
     document.removeEventListener("keydown", Window, false);
   }
 }
+
 function nuevaPalabra() {
   agregarpalabra = document
     .querySelector(".nuevapalabrajuego")
@@ -239,4 +241,23 @@ function mostrarmenuinicio() {
   document.getElementById("caja").style.display = "initial";
   document.getElementById("caja-2").style.display = "none";
   document.getElementById("caja-3").style.display = "none";
+}
+
+function soloLetras(e) {
+  var key = e.keyCode || e.which,
+    tecla = String.fromCharCode(key).toLowerCase(),
+    letras = " áéíóúabcdefghijklmnñopqrstuvwxyz",
+    especiales = [8, 37, 39, 46],
+    tecla_especial = false;
+
+  for (var i in especiales) {
+    if (key == especiales[i]) {
+      tecla_especial = true;
+      break;
+    }
+  }
+
+  if (letras.indexOf(tecla) == -1 && !tecla_especial) {
+    return false;
+  }
 }
